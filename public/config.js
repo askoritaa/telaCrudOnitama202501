52a281e41a52088afa5be20bc09b59bed6f83cc8
Lucas Silva <askoritaa@gmail.com>
2025-05-22 21:01:45 -0300

    adicionando CORS

--- public/config.js
+++ public/config.js
@@ -1,14 +1,31 @@
+// export const types = {
+//     pecas: {
+//         apiPath: "/skins",
+//         singlePath: "/skin",
+//         title: "Peças",
+//         columns: ["Id", "Nome do Conjunto", "Preço", "Skin Peão", "Skin Rei", "Ação"],
+//         render: "renderPecas",
+//     },
+//     backgrounds: {
+//         apiPath: "/backgrounds",
+//         singlePath: "/background",
+//         title: "Backgrounds",
+//         columns: ["Id", "Nome do Background", "Prévia", "Ação"],
+//         render: "renderBackgrounds",
+//     }
+// }
+
 export const types = {
     pecas: {
-        apiPath: "/skins",
-        singlePath: "/skin",
+        apiPath: "http://localhost:3000/skins",
+        singlePath: "http://localhost:3000/skin",
         title: "Peças",
         columns: ["Id", "Nome do Conjunto", "Preço", "Skin Peão", "Skin Rei", "Ação"],
         render: "renderPecas",
     },
     backgrounds: {
-        apiPath: "/backgrounds",
-        singlePath: "/background",
+        apiPath: "http://localhost:3000/backgrounds",
+        singlePath: "http://localhost:3000/background",
         title: "Backgrounds",
         columns: ["Id", "Nome do Background", "Prévia", "Ação"],
         render: "renderBackgrounds",
